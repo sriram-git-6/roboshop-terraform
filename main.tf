fd86c2e7d1f84bd2d83241279e70c9c8df9f41cb
sriram <sriram.mudunuru@gmail.com>
2023-08-21 18:20:24 +0530

    terraform

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ variable "test" {}
 output "test" {
   value = var.test
 }
+
+
+# terraform init -backend-config=env-dev/state.tfvars
+# terraform apply -auto-approve -var-file=env-dev/main.tfvars
+
+# delete the .terraform folder to run below commands because u are here changing the environment.
+
+# terraform init -backend-config=env-prod/state.tfvars
+# terraform apply -auto-approve -var-file=env-prod/main.tfvars
